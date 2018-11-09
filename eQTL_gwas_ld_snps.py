b34d104f0b8ba5b9f5f5d03246fd112316b6b531
adurtys <adurtys@gmail.com>
2018-11-09 13:47:48 -0500

    adding file for isLD

--- eQTL_gwas_ld_snps.py
+++ eQTL_gwas_ld_snps.py
@@ -1,8 +1,8 @@
 # Date Created: 2 November 2018
-# Date Last Modified: 2 November 2018
+# Date Last Modified: 9 November 2018
 # Execution: python eQTL_gwas_ld_snps.py [1] [2]
 # argv[1] = path to directory containing LD files for all chromosomes (/project/voight_ML/adurtys/eqtl_feature/plink_output/ld_files)
-# argv[2] = eQTL_snplist_filename (uniq_eQTL_snp_rsIDs.txt)
+# argv[2] = list of eQTL snps
 # Description: determines which eQTL snps are in LD with GWAS snps of interest
 # Run Time: 
 
@@ -11,7 +11,7 @@ import sys, os
 
 # read in command-line arguments
 ld_directory = sys.argv[1]
-eQTL_snplist_filename = sys.argv[2]
+eQTL_snp_filename = sys.argv[2]
 
 ld_filenames = os.listdir(ld_directory)
 
@@ -19,14 +19,13 @@ all_ld_snps_dict = {} # key = chromosome, value = dictionary of eQTL/GWAS snps i
 
 for filename in ld_filenames:
 	# store choromosome number
-	chromosome = filename.split('.')[0].strip("LD_")
+	chromosome = filename.split('.')[0].strip("isLD_")
 	filepath = ld_directory + "/" + filename
 
 	file = open(filepath, 'r')
 
-	file.readline() # remove header line
-
 	ld_snps_dict = {} # key = snpA, value = [snpA, snpB, r2]
+	ld_snp_pairs = {}
 
 	for line in file:
 		line = line.rstrip('\r\n')
@@ -38,6 +37,43 @@ for filename in ld_filenames:
 
 		if (r2 == 1) and (snpA != snpB):
 			print "ERROR: r2 = 1, but snps A and B are not the same! snpA:", snpA, "snpB:", snpB
-		elif (r2 != 1):
-			if (snpA not in ld_snps_dict) and (snpA not in ld_snps_dict.values()[1]):
-				ld_snps_dict[snpA] = 
+			# if snpA == snpB, then not in LD because same snp
+		
+		if (r2 != 1):
+			if snpA not in ld_snps_pairs.keys():
+				# check if it is the values (stored as snpB)
+				if snpA in ld_snp_pairs.values():
+					# snpA is stored as snpB in the pairs dictionary --> get snpA
+					origSnpA = ld_snp_pairs.keys()[ld_snp_pairs.values().index(snpA)]
+					# check that r2 is the same
+					if (ld_snps_dict[origSnpA][2] != r2):
+						print "ERROR: r2 values are different for", snpA
+
+				else: #store snpA
+					ld_snps_dict[snpA] = snpB
+					ld_snps_dict[snpA] = [snpA, snpB, r2, chromosome]
+
+	all_ld_snps_dict[chromosome] = ld_snps_dict
+
+	file.close()
+	
+eQTL_snp_file = open(eQTL_snp_filename, 'r')
+eQTL_snpList = []
+for line in eQTL_snp_file:
+	eQTL_snp = line.rstrip('\r\n')
+	eQTL_snpList.append(eQTL_snp)
+eQTL_snp_file.close()
+
+tab = "\t"
+newline = "\n"
+
+output = ""
+
+for snp in eQTL_snpList:
+	for chromosome in all_ld_snps_dict:
+		if (snp in all_ld_snps_dict[chromosome]):
+			for i in range(len(all_ld_snps_dict[chromosome][snp])):
+				if i < len(all_ld_snps_dict[chromosome][snp] - 1):
+					output += all_ld_snps_dict[chromosome][snp][i] + tab
+				else:
+					output += all_ld_snps_dict[chromosome][snp][i] + newline
