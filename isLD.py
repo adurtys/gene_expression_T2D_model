1ecf62a9c4874261555476c9c84a5b1ac4c5d836
adurtys <adurtys@gmail.com>
2019-04-29 18:26:12 -0400

    debugging

--- isLD.py
+++ isLD.py
@@ -63,52 +63,7 @@ for line in ld_file:
 
 		output += snpA_position + tab + snpA_rsID + tab + snpB_position + tab + snpB_rsID + newline
 
-outFilename = "isLD.txt"
+outFilename = "isLD_2.txt"
 outFile = open(outFilename, 'w')
 outFile.write(output)
 outFile.close()
-
-
-
-# 		if snpA_rsID in isLD_dict: # eQTL snp is in LD w/ more than one GWAS snp
-# 			if snpA_rsID in isLD_multipleLinkedSnps_dict: # eQTL snp is already in multipleLinkedSnps dict --> just add the new linked gwas snp
-# 				isLD_multipleLinkedSnps_dict[snpA_rsID].append(snpB_position)
-# 				isLD_multipleLinkedSnps_dict[snpA_rsID].append(snpB_rsID)
-# 			else: # add eQTL snp to the isLD_multipleLinkedSnps_dict
-# 				isLD_multipleLinkedSnps_dict[snpA_rsID] = [snpA_position, snpA_rsID, isLD_dict[snpA_rsID][2], isLD_dict[snpA_rsID][3], snpB_position, snpB_rsID]
-
-# 			# remove eQTL snp from the isLD
-# 			del isLD_dict[snpA_rsID]
-
-# 		else:
-# 			isLD_dict[snpA_rsID] = [snpA_position, snpA_rsID, snpB_position, snpB_rsID]
-# ld_file.close()
-
-# print "There are", numColocalizedSnps, "colocalizing snps."
-# print "There are", len(isLD_multipleLinkedSnps_dict), "eQTL snps that are linked with more than one GWAS snp."
-
-# # create output file for every eQTL snp that is in LD with a GWAS snp
-# tab = "\t"
-# newline = "\n"
-
-# output = "CHR_A_BP_A_" + tab + "SNP_A" + tab + "CHR_B_BP_B_" + tab + "SNP_B" + newline
-
-# for snp in isLD_dict:
-# 	for i in range(len(isLD_dict[snp])):
-# 		if i < (len(isLD_dict[snp]) - 1):
-# 			output += isLD_dict[snp][i] + tab
-# 		else:
-# 			output += isLD_dict[snp][i] + newline
-
-# for snp in isLD_multipleLinkedSnps_dict:
-# 	numLinkedGwasSnps = (len(isLD_multipleLinkedSnps_dict[snp]) - 2) / 2
-# 	for i in range(numLinkedGwasSnps):
-# 		output += isLD_multipleLinkedSnps_dict[snp][0] + tab + isLD_multipleLinkedSnps_dict[snp][1] + tab
-		
-# 		newIndex = (i + 1) * 2
-# 		output += isLD_multipleLinkedSnps_dict[snp][newIndex] + tab + isLD_multipleLinkedSnps_dict[snp][newIndex + 1] + newline
-
-# outFilename = "isLD.txt"
-# outFile = open(outFilename, 'w')
-# outFile.write(output)
-# outFile.close()
