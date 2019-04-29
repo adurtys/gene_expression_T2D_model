cf0f3beb8be3ccd8ac50dbba21866c48ce72c677
adurtys <adurtys@gmail.com>
2019-04-29 17:34:17 -0400

    debugging isQTL

--- isQTL.py
+++ isQTL.py
@@ -70,17 +70,6 @@ print "There are", numSnpGroups, "groups."
 # for each snpGroup, determine whether any of its snps are or are associated with eQTLs
 isQTL_dict = {} # key = snpGroup, value = 1 or 0 (1 if any of the snps in the snpGroup is/is associated with an eQTL)
 
-# debug
-for i in range(5):
-	print "snpGroupsDict.keys()[i]:", snpGroupsDict.keys()[i]
-
-key1 = snpGroupsDict.keys()[0]
-print "key1 = snpGroupsDict.keys()[0] =", key1
-print "len(snpGroupsDict[key1]):", len(snpGroupsDict[key1])
-for i in range(len(snpGroupsDict[key1])):
-	print snpGroupsDict[key1][i]
-
-
 num_isQTLs = 0
 for group in snpGroupsDict:
 	isQTL_dict[group] = 0
@@ -101,7 +90,7 @@ for snpGroup in isQTL_dict:
 	output += snpGroup + tab + str(isQTL_dict[snpGroup]) + newline
 
 output_filename = "isQTL.txt"
-output_file = open(output, 'w')
+output_file = open(output_filename, 'w')
 output_file.write(output)
 output_file.close()
 
